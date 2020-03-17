d95919cbe1136996c5256956e222b972e7cac2aa
Bharathi-Bala <61871018+Bharathi-Bala@users.noreply.github.com>
2020-03-17 15:13:17 +0530

    Update removeString.java

--- removeString.java
+++ removeString.java
@@ -1,14 +1,35 @@
+import java.util.Arrays;
 import java.util.Scanner;
-class RemoveString
+ class RemoveWordFromSentence
 {
-	public static void main(String arg[])
-	{
-		Scanner input=new Scanner(System.in);
-		System.out.println("Enter the Sentence ?");
-		String sentence=input.nextLine();
-		System.out.println("Enter the String to be removed ?");
-		String remove=input.nextLine();
-	    String removedString=sentence.replace(remove,"");
-	    System.out.println(removedString);
-	}
+  public static void main(String ar[])
+  {
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter the Sentence  ?");
+    String sentence=input.nextLine();
+    System.out.println("Enter the word/letter to be removed ?");
+    String remove=input.nextLine();
+    
+
+    if(sentence.contains(remove))
+    {
+      String output="";
+      String resArr[];
+      resArr=sentence.split(remove);
+      for(int i=0;i<resArr.length;i++)
+      {
+        output+=resArr[i];
+      }
+      System.out.println(output);
+    }
+    else
+    {
+      System.out.println("The Sentence didn't contain the word to be removed !");
+    }
+
+
+    //another way using replace;
+    String replace=sentence.replace(remove,"");
+    System.out.println(replace);
+  }
 }
