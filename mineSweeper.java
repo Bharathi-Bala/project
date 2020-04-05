234f1b782d83dd02f7db395f392f04bacbdb6017
Bharathi-Bala <61871018+Bharathi-Bala@users.noreply.github.com>
2020-04-05 11:43:47 +0530

    Add files via upload

--- mineSweeper.java
+++ mineSweeper.java
@@ -1,25 +1,44 @@
 import java.util.*;
-public class mineSweeper{
-	int rows=5;
-	int cols=5;
-	int[][] boardValue=new int[rows][cols];
-	String[][] boardShown=new String[rows][cols];
+class mineSweeperWork{
+	int rows,cols,mines,cellCount,cellCntExclBomb;
+	int[][] boardValue;
+	String[][] boardShown;
 	boolean bombBlast;
-	
-	HashMap<Integer,Integer> unTraversed=new HashMap<Integer,Integer>();
-	public mineSweeper(){
-		boardValue[0][3]=-1;
-		
-		boardValue[3][1]=-1;
-		
+	ArrayList<String> unTraversed=new ArrayList<String>();
+	ArrayList<String> traversed=new ArrayList<String>();
+	Scanner input=new Scanner(System.in);
+	public mineSweeperWork(){
+		System.out.println("Enter the no of rows : ");
+		rows=input.nextInt();
+		System.out.println("Enter the no of columns : ");
+		cols=input.nextInt();
+		System.out.println("Enter the no of mines for the given rows and columns : ");
+		mines=input.nextInt();
+		cellCount=rows*cols;
+		cellCntExclBomb=cellCount-mines;
+		boardValue=new int[rows][cols];
+		boardShown=new String[rows][cols];
+		for(int i=0;i<mines;i++)
+        {
+            Random randomBombs = new Random();
+             int r = randomBombs.nextInt(rows);
+            int c = randomBombs.nextInt(cols);
+            if(boardValue[r][c]!=-1)
+            {
+                boardValue[r][c]=-1;
+            }
+            else
+            {
+                i-=1;
+            }
+        }		
 		for(int i=0;i<boardValue.length;i++){
 			for(int j=0;j<boardValue.length;j++){
 				if(boardValue[i][j]!=-1){
 					boardValue[i][j]=0;
 				}
 			}
-		}
-		
+		}		
 		for(int i=0;i<boardValue.length;i++){
 			for(int j=0;j<boardValue.length;j++){
 				if(boardValue[i][j]==-1){
@@ -30,224 +49,190 @@ public class mineSweeper{
 				}
 			}
 		}
-		print(boardValue);
 		for(int i=0;i<boardShown.length;i++){
 			for(int j=0;j<boardShown.length;j++){
 				boardShown[i][j]=".";
 			}
-		}
-		viewMineSweeper(boardShown);
+		}		
+		System.out.println();
+		viewMineSweeper(boardShown,false);
 	}
 	public void neighbour(int i,int j){
-		
-
-		 	if (i < rows && j < cols && i >= 1 && j >= 1 && boardValue[i-1][j-1]!=-1) {
-               boardValue[i-1][j-1]=boardValue[i-1][j-1]+1;
-
-            }
-          	if (i < rows && j < cols-1 && i >= 1 && j >= 0 && boardValue[i-1][j+1]!=-1) {
-                boardValue[i-1][j+1]=boardValue[i-1][j+1]+1;
-            }
-            if (i < rows-1 && j < cols && i >= 0 && j >= 1 && boardValue[i+1][j-1]!=-1 ) {
-               	boardValue[i+1][j-1]=boardValue[i+1][j-1]+1;
-            }
-            if (i < rows-1 && j < cols-1 && i >= 0 && j >= 0 && boardValue[i+1][j+1]!=-1 ) {
-                boardValue[i+1][j+1]=boardValue[i+1][j+1]+1;
-            }
-            if (i < rows && j < cols && i >= 1 && j >= 0 &&  boardValue[i-1][j]!=-1) {
-                boardValue[i-1][j]=boardValue[i-1][j]+1;
-       		 }
-			if(i < rows && j < cols && i >= 0 && j >= 1 && boardValue[i][j-1]!=-1) {
-	            boardValue[i][j-1]=boardValue[i][j-1]+1;
-	        }
-	        if (i < rows && j < cols-1 && i >= 0 && j >= 0 && boardValue[i][j+1]!=-1) {
-	            boardValue[i][j+1]=boardValue[i][j+1]+1;
-	        }
-
-	        if (i < rows-1 && j < cols && i >= 0 && j >= 0 &&  boardValue[i+1][j]!=-1) {
-	            boardValue[i+1][j]=boardValue[i+1][j]+1;
-	          }
-
+	 	if (i < rows && j < cols && i >= 1 && j >= 1 && boardValue[i-1][j-1]!=-1) {
+           boardValue[i-1][j-1]=boardValue[i-1][j-1]+1;
+        }
+      	if (i < rows && j < cols-1 && i >= 1 && j >= 0 && boardValue[i-1][j+1]!=-1) {
+            boardValue[i-1][j+1]=boardValue[i-1][j+1]+1;
+        }
+        if (i < rows-1 && j < cols && i >= 0 && j >= 1 && boardValue[i+1][j-1]!=-1 ) {
+           	boardValue[i+1][j-1]=boardValue[i+1][j-1]+1;
+        }
+        if (i < rows-1 && j < cols-1 && i >= 0 && j >= 0 && boardValue[i+1][j+1]!=-1 ) {
+            boardValue[i+1][j+1]=boardValue[i+1][j+1]+1;
+        }
+        if (i < rows && j < cols && i >= 1 && j >= 0 &&  boardValue[i-1][j]!=-1) {
+            boardValue[i-1][j]=boardValue[i-1][j]+1;
+   		}
+		if(i < rows && j < cols && i >= 0 && j >= 1 && boardValue[i][j-1]!=-1) {
+            boardValue[i][j-1]=boardValue[i][j-1]+1;
+        }
+        if (i < rows && j < cols-1 && i >= 0 && j >= 0 && boardValue[i][j+1]!=-1) {
+            boardValue[i][j+1]=boardValue[i][j+1]+1;
+        }
+        if (i < rows-1 && j < cols && i >= 0 && j >= 0 &&  boardValue[i+1][j]!=-1) {
+            boardValue[i+1][j]=boardValue[i+1][j]+1;
+        }
+	}
+	public void neighbourBlankCell(int x,int y){
+	 	if(boardValue[x][y]!=0){
+           		boardShown[x][y]=Integer.toString(boardValue[x][y]);
+           }
+           else{
+           		boardShown[x][y]="$";
+           		if(traversed.contains(x+","+y)==false && unTraversed.contains(x+","+y)==false){
+           			unTraversed.add(x+","+y);
+				}
+           }
 	}
 	public int blankAdjacentCell(int i,int j){
-			boardShown[i][j]="$";
-
-		 	if (i < rows && j < cols && i >= 1 && j >= 1  ) {
-               if(boardValue[i-1][j-1]!=0){
-               		boardShown[i-1][j-1]=Integer.toString(boardValue[i-1][j-1]);
-               }
-               else{
-               		boardShown[i-1][j-1]="$";
-               		unTraversed.put(i-1,j-1);
-               }
-
-            }
-          	if (i < rows && j < cols-1 && i >= 1 && j >= 0 ) {
-               
-                if(boardValue[i-1][j+1]!=0){
-               		boardShown[i-1][j+1]=Integer.toString(boardValue[i-1][j+1]);
-               }
-               else{
-               	boardShown[i-1][j+1]="$";
-               		unTraversed.put(i-1,j+1);
-               }
-            }
-            if (i < rows-1 && j < cols && i >= 0 && j >= 1  ) {
-               	
-               	if(boardValue[i+1][j-1]!=0){
-               		boardShown[i+1][j-1]=Integer.toString(boardValue[i+1][j-1]);
-               }
-               else{
-               	boardShown[i+1][j-1]="$";
-               		unTraversed.put(i+1,j-1);
-               }
-            }
-            if (i < rows-1 && j < cols-1 && i >= 0 && j >= 0  ) {
-               
-                if( boardValue[i+1][j+1]!=0){
-               		boardShown[i+1][j+1]= Integer.toString(boardValue[i+1][j+1]);
-               }
-               else{
-               	boardShown[i+1][j+1]="$";
-               		unTraversed.put(i+1,j+1);
-               }
-            }
-            if (i < rows && j < cols && i >= 1 && j >= 0) {
-              
-                if(boardValue[i-1][j]!=0){
-               		boardShown[i-1][j]=Integer.toString(boardValue[i-1][j]);
-               }
-               else{
-               	boardShown[i-1][j]="$";
-               		unTraversed.put(i-1,j);
-               }
-       		 }
-			if(i < rows && j < cols && i >= 0 && j >= 1) {
-	            
-	            if( boardValue[i][j-1]!=0){
-               		boardShown[i][j-1]= Integer.toString(boardValue[i][j-1]);
-               }
-               else{
-               	boardShown[i][j-1]="$";
-               		unTraversed.put(i,j-1);
-               }
-	        }
-	        if (i < rows && j < cols-1 && i >= 0 && j >= 0) {
-	           
-	            if(boardValue[i][j+1]!=0){
-               		boardShown[i][j+1]=Integer.toString(boardValue[i][j+1]);
-               }
-               else{
-               	boardShown[i][j+1]="$";
-               		unTraversed.put(i,j+1);
-               }
-	        }
-
-	        if (i < rows-1 && j < cols && i >= 0 && j >= 0) {
-	           
-	            if(boardValue[i+1][j]!=0){
-               		boardShown[i+1][j]=Integer.toString(boardValue[i+1][j]);
-               }
-               else{
-               	boardShown[i+1][j]="$";
-               		unTraversed.put(i+1,j);
-               }
-	        }
-
-	        System.out.println("unTraversed Size : "+unTraversed.size());
-
-	        return unTraversed.size();
+		traversed.add(i+","+j);			
+		boardShown[i][j]="$";
+	 	if (i < rows && j < cols && i >= 1 && j >= 1  ) {              
+	 		neighbourBlankCell(i-1,j-1);
+        }
+      	if (i < rows && j < cols-1 && i >= 1 && j >= 0 ) {
+      		neighbourBlankCell(i-1,j+1);
+        }
+        if (i < rows-1 && j < cols && i >= 0 && j >= 1  ) {
+        	neighbourBlankCell(i+1,j-1);
+        }
+        if (i < rows-1 && j < cols-1 && i >= 0 && j >= 0  ) {
+        	neighbourBlankCell(i+1,j+1);
+        }
+        if (i < rows && j < cols && i >= 1 && j >= 0) {
+        	neighbourBlankCell(i-1,j);
+   		 }
+		if(i < rows && j < cols && i >= 0 && j >= 1) {
+			neighbourBlankCell(i,j-1);
+        }
+        if (i < rows && j < cols-1 && i >= 0 && j >= 0) {
+        	neighbourBlankCell(i,j+1);
+        }
+        if (i < rows-1 && j < cols && i >= 0 && j >= 0) {
+        	neighbourBlankCell(i+1,j);
+        }
+	    return unTraversed.size();
 	}
 	
 	public void print(int[][] array){
 		for(int i=0;i<array.length;i++){
 			for(int j=0;j<array.length;j++){
-				System.out.print(array[i][j]+" ");
+				System.out.print(array[i][j]+"\t");
 			}
 			System.out.println();
 		}
 	}
-	public void viewMineSweeper(String[][] array){
-		for(int i=0;i<array.length;i++){
+	public void viewMineSweeper(String[][] array,boolean bombBlast){
+		 for(int i=0;i<cols;i++){
+                System.out.print("\t"+i);
+            }
+            System.out.println("\n\n");
+			for(int i=0;i<array.length;i++){
+			System.out.print(i+"\t");
 			for(int j=0;j<array.length;j++){
-				System.out.print(array[i][j]+" ");
+				System.out.print(array[i][j]+"\t");
 			}
-			System.out.println();
+			System.out.println("\n\n");
+		}
+		winningCondition(array,bombBlast);
+	}
+	public void winningCondition(String[][] finalarr,boolean bombBlast){
+		int openCell=0;
+		for(int i=0;i<finalarr.length;i++){
+			for(int j=0;j<finalarr.length;j++){
+				if(boardShown[i][j]!="."){
+					openCell+=1;
+				}
+			}
+		}
+		if(openCell==cellCntExclBomb && bombBlast==false){
+			System.out.println("You wins !  ");
+			System.exit(0);
 		}
 	}
 	public void checkCondition(int x,int y,String f){
-		if(boardValue[x][y]!=-1 && boardValue[x][y]!=0 && f.equals("n") && boardShown[x][y]!="F"){
+		if(boardValue[x][y]!=-1 && boardValue[x][y]!=0 && f.equals("n") && boardShown[x][y]!="F" && boardShown[x][y]=="."){
 			boardShown[x][y]=Integer.toString(boardValue[x][y]);
-			viewMineSweeper(boardShown);
 			bombBlast=false;
-			
+			viewMineSweeper(boardShown,bombBlast);			
+		}
+		else if(boardValue[x][y]!=-1  && (f.equals("n") || f.equals("y")) && boardShown[x][y]!="F" && boardShown[x][y]!="."){
+			System.out.println("Already the given position is opened ! ...");
+			bombBlast=false;
 		}
 		else if(boardValue[x][y]==0 && f.equals("n") && boardShown[x][y]!="F"){
 			int size=blankAdjacentCell(x,y);
-			if(size>0){
-				System.out.println(unTraversed);
-				Iterator iter = unTraversed.entrySet().iterator();
-		        while (iter.hasNext()) {
-		            Map.Entry entry = (Map.Entry) iter.next();
-		            int key=(int)entry.getKey();
-		            int value=(int)entry.getValue();
-		            size=blankAdjacentCell(key,value);
-
-		        }
-		        viewMineSweeper(boardShown);
-		    }
-			else{
-				viewMineSweeper(boardShown);
-				bombBlast=false;
-			}
-			
-			
+			while(size>0){
+				for(int i=0;i<size;i++){
+					String ar=unTraversed.get(i);
+		           	String[] arra=ar.split(",");
+		            unTraversed.remove(i);
+		            size=blankAdjacentCell(Integer.parseInt(arra[0]),Integer.parseInt(arra[1]));
+				}
+		    }			
+			bombBlast=false;
+			viewMineSweeper(boardShown,bombBlast);
 		}
 		else if(boardShown[x][y]=="F" && f.equals("n")){
-			   boardShown[x][y]=".";
-				viewMineSweeper(boardShown);
-				bombBlast=false;
+			boardShown[x][y]=".";
+			bombBlast=false;
+			viewMineSweeper(boardShown,bombBlast);
+			
 		}
-		else if(boardShown[x][y]=="." &&f.equals("y")){
-			
-				boardShown[x][y]="F";
-				viewMineSweeper(boardShown);
-				bombBlast=false;
-			
+		else if(boardShown[x][y]=="." &&f.equals("y")){			
+			boardShown[x][y]="F";
+			bombBlast=false;
+			viewMineSweeper(boardShown,bombBlast);
+						
+		}
+		else if(boardShown[x][y]=="F" &&f.equals("y")){			
+			System.out.println("Already the given position has been flagged .");
+			bombBlast=false;			
 		}
 		else if(boardValue[x][y]==-1 && f.equals("n")){
 			for(int i=0;i<boardValue.length;i++){
 				for(int j=0;j<boardValue.length;j++){
 					if(boardValue[i][j]==-1){
-						boardShown[i][j]="*";
-						
-						
+						boardShown[i][j]="B";
 					}
 				}
 			}
-			viewMineSweeper(boardShown);
-			System.out.println("Game Over .....");
 			bombBlast=true;
-		}
-		
+			viewMineSweeper(boardShown,bombBlast);
+			System.out.println("Game Over .....");
+			
+		}		
 	}
-	public void playGame(){
+	public void playGame(){		    
 		do{
-			Scanner input=new Scanner(System.in);
-			System.out.println("Enter the row");
+			System.out.println("Enter the row between 0 and "+rows);
 			int x=input.nextInt();
-			System.out.println("Enter the column");
+			System.out.println("Enter the column between 0 and "+cols);
 			int y=input.nextInt();
 			System.out.println("Do you want make this position as flag ? say(y/n)");
 			String f=input.next();
-			checkCondition(x,y,f);
+			if(x>=0 && x<rows && y>=0 && y<cols){
+				checkCondition(x,y,f);
+			}
+			else{
+				System.out.println("The given postion doesn't fall between the range");
+			}
 		}while(bombBlast==false);
 	}
 }
-class mineSweeperGame{
+class MineSweeperDemo{
 	public static void main(String arg[]){
-		mineSweeper mine=new mineSweeper();
+		mineSweeperWork mine=new mineSweeperWork();
 		mine.playGame();
 	}
 }
-
